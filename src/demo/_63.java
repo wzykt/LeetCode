9f97d62b8ef8ade233b470c3650349a7f26cf338
wuzongyu <ktsvook@gmail.com>
2021-09-06 23:03:14 +0800

    ‘2021/9/6 LeetCode 63’

--- src/demo/_63.java
+++ src/demo/_63.java
@@ -6,18 +6,37 @@ package demo;
  * @date 2021/8/28
  * @decription
  * @result 暴力递归解出来了，但是没有转化成动态规划
+ * 2021/9/6 dp解决
  */
 public class _63 {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if (obstacleGrid == null) {
             return 0;
         }
-        int rows = obstacleGrid.length - 1;//行数
-        int columns = obstacleGrid[0].length - 1;//列数
-
-        int result = 0;
-        result = process(obstacleGrid, 0, 0, rows, columns);
-        return result;
+        int row = obstacleGrid.length;
+        int column = obstacleGrid[0].length;
+        int dp[][] = new int[row + 1][column + 1];
+        dp[row - 1][column - 1] = 1;
+        for (int i = row; i >= 0; i--) {
+            for (int j = column; j >= 0; j--) {
+                if (i < row && j < column && obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if (i == row - 1 && j == column - 1) {
+                    continue;
+                }
+                if (i < row && j < column) {
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+        //        int rows = obstacleGrid.length - 1;//行数
+//        int columns = obstacleGrid[0].length - 1;//列数
+//        int result = 0;
+//        result = process(obstacleGrid, 0, 0, rows, columns);
+//        return result;
     }
 
     //暴力递归，超时
