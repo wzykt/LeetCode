d60e5f0a6ced2feac122652e99a3301dc6ffa4bb
wuzongyu <ktsvook@gmail.com>
2021-09-09 22:23:07 +0800

    ‘2021/9/9 LeetCode 100’

---
+++ src/demo/_100.java
@@ -0,0 +1,42 @@
+package demo;
+
+/**
+ * 100. 相同的树
+ * @date 2021/9/9
+ */
+public class _100 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null && q != null) {
+            return false;
+        }
+        if (p != null && q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
